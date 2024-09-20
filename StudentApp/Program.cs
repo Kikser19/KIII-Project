1cf77b0dc606d8664cf39cbcf4bb2bdd7b61ba24
Kristijan <angelkovskikristijan3@gmail.com>
2024-09-21 01:25:01 +0200

    Initial commit

---
+++ StudentApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApp.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StudentAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAppContext"), sql=>sql.MigrationsAssembly(typeof(Program).Assembly.GetName().Name)));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
